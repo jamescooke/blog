99e224dba4baf3ddbeceb5bc526649e397427bdb
James Cooke <james@fublo.net>
2013-01-20 13:26:38 +0000

    Initial Pelican build out.

---
+++ pelicanconf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'James'
+SITENAME = u'James Cooke Dot Info'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
